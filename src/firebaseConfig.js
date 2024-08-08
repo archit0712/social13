351a644a395633b981621ee0ec476b37a2e0a5fc
Archit Mittal <e19cse041@gmail.com>
2024-08-08 19:53:27 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getAuth, signOut } from "@firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDK_tnVan82kk-7dp31gmQQGSbbEl_d4Qg",
-  authDomain: "social13-0712.firebaseapp.com",
-  projectId: "social13-0712",
-  storageBucket: "social13-0712.appspot.com",
-  messagingSenderId: "763193478927",
-  appId: "1:763193478927:web:a2f438aa9d6a7bacc2787a",
-  measurementId: "G-HWKJ1TSVPL",
+  apiKey: //api key,
+  authDomain: //auth domain,
+  projectId: //project id,
+  storageBucket: //storage bucket,
+  messagingSenderId: // messaging ID,
+  appId: // app Id,
+  measurementId: // measurement ID,
 };
 
 // Initialize Firebase
